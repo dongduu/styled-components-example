e8cd8256b6b525e25ac80c3cf238392e96a2c84c
dongduu <4012popo@naver.com>
2022-01-10 17:25:09 +0900

    feat: Styled Components(1)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import StyledButton from "./components/StyledButton";
 
 function App() {
   return (
@@ -7,16 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <StyledButton>버튼</StyledButton>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
